429055816ef0d4be152b612d5ac8ce2d9cf76c91
@bhi_yanta116 <abhinawjai123@gmail.com>
2026-07-24 01:05:10 +0530

    Sliding Window of brainnn

--- Prefix Sum/06_LongestSubsequenceWithLimitedSum2389.cpp
+++ Prefix Sum/06_LongestSubsequenceWithLimitedSum2389.cpp
@@ -17,16 +17,30 @@ vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
             nums[i] += nums[i-1];
         }
         for (int i = 0; i < m; i++) { // T.C - O(m*n)
-            int len = 0;
-            for (int j = 0; j < n; j++){
-                if(nums[j] > queries[i]) break;
-                len++;
+            int maxlen = 0;
+            // method 1: Not Efficient
+            // for (int j = 0; j < n; j++){
+            //     if(nums[j] > queries[i]) break;
+            //     maxlen++;
+
+            // Method 2:
+            int lo = 0;
+            int hi = n - 1;
+            while (lo <= hi) {
+                int mid = lo + (hi - lo) / 2;
+                if (nums[mid] > queries[i])
+                    hi = mid - 1;
+                else {
+                    maxlen = mid + 1;
+                    lo = mid + 1;
+                }
             }
-            ans[i] = len ;
+            ans[i] = maxlen;
         }
         return ans;
     }
-    // Total Time Complexity - O(nlogn + m*n).
+    // Total Time Complexity - O(nlogn + m*n);
+    // Total Time Complexity - O((m+n)logn);
 
 
 int main(){
