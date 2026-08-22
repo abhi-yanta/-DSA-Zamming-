bdcd31ea52fd50538234de02376b40b3fc5ccd00
Abhinav Jaiswal <2508410100001@recsonbhadra.ac.in>
2026-08-22 14:56:02 +0530

    Add files via upload

---
+++ OOPs (Love Babbar)/15_Friend Keyword/BetterApproach.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+
+using namespace std;
+
+class A{
+
+private:
+    int x;
+
+public:
+    A(int _val): x(_val){}
+    int getx()const {return x;}
+    void setx(int _val){ x = _val;}
+
+    void print() const {
+        cout << x << endl;
+    }
+
+    // friend class B; // By this we can now access 
+    //                 // the private variable x of class A in class B
+
+    // friend void print(const A &); // Same can be directly done with the function also
+                   
+};
+
+class B
+{
+
+public:
+    void print (const A &a){
+        // cout << a.getx()<< endl;
+        // cout << a.x << endl;
+        a.print();
+
+    }    
+};
+
+// void print (const A &a){
+//     cout << a.x << endl;
+// }
+
+int main(){
+    A a(5);
+    B b;
+    b.print(a);
+    
+
+    return 0;
+}
+
+// # Note:
+    // Previous intro approach is not secure as we are making our private variable can be accessed.
